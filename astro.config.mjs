50cff2e81abf4e8241b19060cbfc85b451aa305f
gaston <gaston.isnardi@cloudhesive.com>
2024-12-05 17:01:52 -0300

    add new map Section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
