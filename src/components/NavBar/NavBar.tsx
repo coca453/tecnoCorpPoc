d358f5272794ee87a5f9bab4a1cacb4386742c3e
gaston <gaston.isnardi@cloudhesive.com>
2024-12-09 19:00:09 -0300

    final web

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import {
+  Dialog,
+  DialogPanel,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/20/solid";
 
 const navigation = [
-  { name: "Quiénes somos", href: "about" },
-  { name: "Servicios", href: "servicios" },
-  { name: "Clientes", href: "clientes" },
-  { name: "Certificaciones", href: "certificaciones" },
-  { name: "Contacto", href: "contacto" },
+  { name: "Quiénes somos", href: "/about" },
+  { name: "Servicios", href: "/servicios" },
+  { name: "Clientes", href: "/clientes" },
+  {
+    name: "Certificaciones",
+    href: "/certificaciones",
+  },
+  { name: "Contacto", href: "/contacto" },
 ];
 
 export const NavBar = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [hasScrolled, setHasScrolled] = useState(false);
-  const [currentPath, setCurrentPath] = useState("");
+  const [mobileMenuOpen, setMobileMenuOpen] =
+    useState(false);
+  const [hasScrolled, setHasScrolled] =
+    useState(false);
+  const [currentPath, setCurrentPath] =
+    useState("");
   useEffect(() => {
     // Asegura que `window.location` esté disponible en el cliente
     setCurrentPath(window.location.pathname);
@@ -31,8 +46,15 @@ export const NavBar = () => {
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener(
+      "scroll",
+      handleScroll
+    );
+    return () =>
+      window.removeEventListener(
+        "scroll",
+        handleScroll
+      );
   }, []);
 
   return (
@@ -40,9 +62,13 @@ export const NavBar = () => {
       className={`inset-x-0 top-0 z-50 transition-colors duration-300 p-5 bg-primary`}
     >
       <div className="mx-auto max-w-7xl lg:w-full">
-        <div className={`px-6 pt-6 lg:pl-6 lg:pr-0 flex`}>
+        <div
+          className={`p-1 lg:px-6 lg:pt-6 lg:pl-6 lg:pr-0 flex justify-between`}
+        >
           <a href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">
+              Your Company
+            </span>
             <img
               alt="tecnaCorp Logo"
               src="/assets/img/LogoMono.webp"
@@ -51,15 +77,22 @@ export const NavBar = () => {
           </a>
           <nav
             aria-label="Global"
-            className={`flex items-center justify-between ${currentPath !== "/" ? "lg:justify-center" : "lg:justify-start"}`}
+            className={`flex items-center justify-between`}
           >
             <button
               type="button"
-              onClick={() => setMobileMenuOpen(true)}
+              onClick={() =>
+                setMobileMenuOpen(true)
+              }
               className="-m-2.5 rounded-md p-2.5  lg:hidden"
             >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+              <span className="sr-only">
+                Open main menu
+              </span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="h-6 w-6 text-secondary"
+              />
             </button>
             <div
               className={`hidden lg:ml-16 lg:flex lg:gap-x-14 lg:w-full lg:justify-around`}
@@ -83,19 +116,32 @@ export const NavBar = () => {
         className="lg:hidden"
       >
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-primary px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img alt="" src="/assets/img/logo.webp" className="h-8 w-auto" />
+              <span className="sr-only">
+                Your Company
+              </span>
+              <img
+                alt=""
+                src="/assets/img/LogoMono.webp"
+                className="h-10 w-auto"
+              />
             </a>
             <button
               type="button"
-              onClick={() => setMobileMenuOpen(false)}
+              onClick={() =>
+                setMobileMenuOpen(false)
+              }
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+              <span className="sr-only">
+                Close menu
+              </span>
+              <XMarkIcon
+                aria-hidden="true"
+                className="h-6 w-6 text-secondary"
+              />
             </button>
           </div>
           <div className="mt-6 flow-root">
@@ -105,7 +151,7 @@ export const NavBar = () => {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary-dark hover:bg-gray-50"
                   >
                     {item.name}
                   </a>
