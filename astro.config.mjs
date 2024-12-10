2c9e8d9c704b8ab25c0965a462e1706122bc1553
gaston <gaston.isnardi@cloudhesive.com>
2024-12-10 02:02:13 -0300

    final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()],
+  site: "https://www.tecna-corp.com", // URL base de tu sitio
+  integrations: [
+    tailwind(),
+    react(),
+    icon(),
+    sitemap(),
+  ],
   output: "server",
   adapter: vercel(),
   experimental: {
